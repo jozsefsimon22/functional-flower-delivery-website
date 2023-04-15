dd11a2c31645ee2dfa24ac7b8ce29b997fb65cd5
jozsefsimon22 <nex.simon@gmail.com>
2023-04-15 09:21:48 +0100

    updated footer to php

--- login.php
+++ login.php
@@ -11,37 +11,39 @@
 
 <body>
     <!-- Header -->
-    <div class="container-fluid">
-        <div class="row align-items-center">
-            <div class="col">
-                <img src="Photos\logo.png" alt="company-logo">
-            </div>
-        </div>
+    <?php require 'header.php'; ?>
 
-        <!-- Navigation -->
-        <?php require 'navbar.php'; ?>
+    <!-- Navigation -->
+    <?php require 'navbar.php'; ?>
 
-        <!-- Login -->
-        <div class="center">
-            <div class="container">
-                <a class="close-btn" href="index-home.php">X</a>
-                <form action="#">
-                    <div class="row">
-                        <h1>Login</h1>
-                    </div>
-                    <div class="row">
-                        <label for="emmail">Email</label>
-                        <input type="text" id="email">
-                    </div>
-                    <div class="row">
-                        <label for="password">Password</label>
-                        <input type="password" id="password">
-                    </div>
-                </form>
+    <!-- Login -->
+    <div class="center">
+        <div class="container">
+            <a class="close-btn" href="index-home.php">X</a>
+            <form action="#">
                 <div class="row">
-                    <button>Login</button>
+                    <h1>Login</h1>
                 </div>
+                <div class="row">
+                    <label for="emmail">Email</label>
+                    <input type="text" id="email">
+                </div>
+                <div class="row">
+                    <label for="password">Password</label>
+                    <input type="password" id="password">
+                </div>
+            </form>
+            <div class="row">
+                <button>Login</button>
             </div>
         </div>
+    </div>
+
+    <!-- Footer -->
+    <?php include 'footer.php'; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
 
 </html>
