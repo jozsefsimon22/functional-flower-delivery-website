c92ab282fd380d78a6e8db1b9b34e686ecbe4dc7
jozsefsimon22 <nex.simon@gmail.com>
2023-04-15 10:17:26 +0100

    new login form

--- login.php
+++ login.php
@@ -2,48 +2,65 @@
 <html lang="en">
 
 <head>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+
+    <link rel="stylesheet" href="Style/header.css">
+    <link rel="stylesheet" href="Style/footer.css">
     <link rel="stylesheet" href="Style/login.css">
+
+    <title>Thistle & Bloom Edinburgh</title>
+
 </head>
 
 <body>
     <!-- Header -->
     <?php require 'header.php'; ?>
 
-    <!-- Navigation -->
+    <!-- Navbar -->
     <?php require 'navbar.php'; ?>
 
-    <!-- Login -->
-    <div class="center">
-        <div class="container">
-            <a class="close-btn" href="index-home.php">X</a>
-            <form action="#">
-                <div class="row">
-                    <h1>Login</h1>
-                </div>
-                <div class="row">
-                    <label for="emmail">Email</label>
-                    <input type="text" id="email">
-                </div>
-                <div class="row">
-                    <label for="password">Password</label>
-                    <input type="password" id="password">
-                </div>
-            </form>
+
+    <!-- Login Form -->
+    <div class="login-form" id="test">
+        <h1>Login</h1>
+        <div class="container-fluid">
             <div class="row">
-                <button>Login</button>
+                <div class="col-4">
+                </div>
+                <div class="col-4">
+                    <form action="login.php" method="post">
+                        <label for="username">
+                            <i class="fas fa-user"></i>
+                        </label>
+                        <input type="text" name="username" placeholder="Username" id="username" required>
+                        <label for="password">
+                            <i class="fas fa-lock"></i>
+                        </label>
+                        <input type="password" name="password" placeholder="Password" id="password" required>
+                        <br>
+                        <input type="submit" value="Login">
+                    </form>
+                    <p>Don't have an account? <a href="register.php">Register</a></p>
+                </div>
             </div>
         </div>
-    </div>
 
-    <!-- Footer -->
-    <?php include 'footer.php'; ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-        crossorigin="anonymous"></script>
+        <!-- Footer -->
+        <?php include 'footer.php'; ?>
+
+        <!-- Bootstrap JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+            crossorigin="anonymous"></script>
+
+</body>
 
 </html>
